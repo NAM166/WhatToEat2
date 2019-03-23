466f0c00e808350f09ffcc72abb784e89770918e
Niall <niallmcclelland166@hotmail.com>
2019-03-23 12:51:31 +0000

    23/0319

---
+++ WhatToEat/Migrations/Configuration.cs
@@ -0,0 +1,24 @@
+namespace WhatToEat.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<WhatToEat.Models.ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = true;
+            AutomaticMigrationDataLossAllowed = true;
+        }
+
+        protected override void Seed(WhatToEat.Models.ApplicationDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
